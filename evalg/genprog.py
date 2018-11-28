30a7593f2397fbabcfed88f260a847adc43c44c3
Louis Schlessinger <louschlessinger@gmail.com>
2018-11-28 00:59:31 -0600

    Add GP mutation

--- evalg/genprog.py
+++ evalg/genprog.py
@@ -2,7 +2,10 @@ import copy
 
 import numpy as np
 
-from evalg.encoding import operators, infix_to_postfix, postfix_to_binexp_tree
+from evalg.encoding import operators, infix_to_postfix, postfix_to_binexp_tree, BinaryTreeNode
+
+# todo : get rid of this
+kernels = ['SE1', 'SE2', 'SE3', 'SE4', 'RQ1', 'RQ2', 'RQ3', 'RQ4']
 
 
 def crossover_subtree_exchange(infix_1, infix_2):
@@ -77,3 +80,126 @@ def _select_token_ind(postfix_tokens_1, postfix_tokens_2):
         r2 = np.random.randint(0, len(postfix_tokens_2))
 
     return r1, r2
+
+
+def grow(depth, max_depth=2):
+    """ Grow a random tree
+    """
+    terminals = kernels
+    internals = operators
+    # 2 children for binary trees
+    n_children = 2
+    if depth < max_depth:
+        node = BinaryTreeNode(np.random.choice(terminals + internals))
+
+        if node.value in internals:
+            for i in range(0, n_children):
+                child_i = grow(depth + 1)
+                if not node.left:
+                    node.left = child_i
+                    node.left.parent = node
+                elif not node.right:
+                    node.right = child_i
+                    node.right.parent = node
+    else:
+        node = BinaryTreeNode(np.random.choice(terminals))
+
+    return node
+
+
+def generate_grow(max_depth=2):
+    return grow(depth=0, max_depth=max_depth)
+
+
+def full(depth, max_depth=2):
+    """ Grow a random tree
+    """
+    terminals = kernels
+    internals = operators
+    # 2 children for binary trees
+    n_children = 2
+    if depth < max_depth:
+        node = BinaryTreeNode(np.random.choice(internals))
+
+        if node.value in internals:
+            for i in range(0, n_children):
+                child_i = full(depth + 1)
+                if not node.left:
+                    node.left = child_i
+                    node.left.parent = node
+                elif not node.right:
+                    node.right = child_i
+                    node.right.parent = node
+    else:
+        node = BinaryTreeNode(np.random.choice(terminals))
+
+    return node
+
+
+def generate_full(max_depth=2):
+    return full(depth=0, max_depth=max_depth)
+
+
+def _swap_mut_subtree(node, random_tree):
+    # add mutated subtree to original
+    # swap parents of nodes
+    if node.parent:
+        if node.parent.left is node:
+            node.parent.left = random_tree
+        elif node.parent.right is node:
+            node.parent.right = random_tree
+    random_tree.parent = node.parent
+
+    return node, random_tree
+
+
+def _mutate_subtree_exchange(infix, init_method, **init_method_kwargs):
+    """
+    init_method: initialization method
+    """
+    postfix = infix_to_postfix(infix)
+    tree = postfix_to_binexp_tree(postfix)
+    postfix_tokens = postfix.split(' ')
+
+    r = np.random.randint(0, len(postfix_tokens))
+    node = tree.select_postorder(r)
+
+    random_tree = init_method(init_method_kwargs)
+
+    _swap_mut_subtree(node, random_tree)
+
+    return tree.infix()
+
+
+def mutate_grow(infix, max_depth=2):
+    return _mutate_subtree_exchange(infix, generate_grow, max_depth=max_depth)
+
+
+def mutate_half_and_half(infix, max_depth=2):
+    return _mutate_subtree_exchange(infix, generate_full, max_depth=max_depth)
+
+
+def mutate_point(infix):
+    """Point mutation
+
+    :param infix:
+    :return:
+    """
+    postfix = infix_to_postfix(infix)
+    tree = postfix_to_binexp_tree(postfix)
+    postfix_tokens = postfix.split(' ')
+
+    r = np.random.randint(0, len(postfix_tokens))
+    node = tree.select_postorder(r)
+
+    # change node value to a different value
+    if node.value in kernels:
+        new_val = np.random.choice(list(set(kernels) - {node.value}))
+    elif node.value in operators:
+        new_val = np.random.choice(list(set(operators) - {node.value}))
+    else:
+        raise ValueError('%s not in kernels or operators' % node.value)
+
+    node.value = new_val
+
+    return tree.infix()
