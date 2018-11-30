0b35b448f914baa8b3ec6727fe6145556751a29b
Louis Schlessinger <louschlessinger@gmail.com>
2018-11-30 01:24:10 -0600

    Start adding automatic kernel search

---
+++ autoks/Experiment.py
@@ -0,0 +1,10 @@
+class Experiment:
+
+    def __init__(self):
+        pass
+
+    def kernel_search(self):
+        pass
+
+    def evaluate_models(self):
+        pass
---
+++ autoks/grammar.py
@@ -0,0 +1,10 @@
+def expand():
+    pass
+
+
+def sort_kernel_tree(kernel_tree):
+    pass
+
+
+def remove_duplicates(kernel_tree):
+    pass
