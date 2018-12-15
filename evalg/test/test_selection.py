cc9cc7358b6de40d754be885a4780880d3239cc3
Louis Schlessinger <louschlessinger@gmail.com>
2018-12-15 13:28:12 -0600

    clean up selection test

--- evalg/test/test_selection.py
+++ evalg/test/test_selection.py
@@ -7,19 +7,18 @@ from evalg import selection
 
 class TestSelection(TestCase):
 
+    def setUp(self):
+        self.population = np.array([1, 2, 3, 4, 5])
+        self.k = 3
+        self.fitness_list = np.array([15, 30, 20, 40, 10])
+
     def test_select_k_best(self):
-        population = np.array([1, 2, 3, 4, 5])
-        k = 3
-        fitness_list = np.array([15, 30, 20, 40, 10])
-        result = selection.select_k_best(population, fitness_list, k).tolist()
+        result = selection.select_k_best(self.population, self.fitness_list, self.k).tolist()
         self.assertEqual(result, [3, 4, 2])
 
     def test_select_exponential_ranking(self):
-        population = np.array([1, 2])
-        k = 2
-        fitness_list = np.array([10, 15])
         with self.assertRaises(ValueError):
-            selection.select_exponential_ranking(population, fitness_list, k, c=2)
+            selection.select_exponential_ranking(self.population, self.fitness_list, self.k, c=2)
 
         with self.assertRaises(ValueError):
-            selection.select_exponential_ranking(population, fitness_list, k, c=-1)
+            selection.select_exponential_ranking(self.population, self.fitness_list, self.k, c=-1)
