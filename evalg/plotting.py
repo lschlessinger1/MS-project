3fadefacca4074a112c031aea85676348fe5bbb1
Louis Schlessinger <louschlessinger@gmail.com>
2018-12-15 01:15:37 -0600

    update best so far function

--- evalg/plotting.py
+++ evalg/plotting.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
 
 
-def plot_best_so_far(best_so_far):
+def plot_best_so_far(best_so_far, title='Best-So-Far Curve', x_label='Generation', y_label='Fitness Best So Far'):
     # Display the maximum fitness value at each generation
-    plt.title('Best-So-Far Curve')
+    plt.title(title)
     plt.plot(best_so_far)
-    plt.xlabel('Generation')
-    plt.ylabel('Fitness Best So Far')
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
     plt.show()
