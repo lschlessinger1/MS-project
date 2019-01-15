f7c488340f2a4446ad28a76a84d8c3371991999d
Louis Schlessinger <louschlessinger@gmail.com>
2019-01-14 20:54:31 -0500

    Add BIC

--- autoks/model.py
+++ autoks/model.py
@@ -16,3 +16,19 @@ def model_to_binexptree(model):
     postfix_tokens = infix_tokens_to_postfix_tokens(infix_tokens)
     tree = postfix_tokens_to_binexp_tree(postfix_tokens)
     return tree
+
+
+def BIC(model):
+    """
+    Calculate the Bayesian Information Criterion (BIC) for a GPy `model` with maximum likelihood hyperparameters on a given dataset.
+    https://en.wikipedia.org/wiki/Bayesian_information_criterion
+    """
+    # model.log_likelihood() is the natural logarithm of the marginal likelihood of the Gaussian process.
+    # len(model.X) is the number of data points.
+    # model._size_transformed() is the number of optimisation parameters.
+    # BIC = ln(n)k - 2ln(L^)
+    import numpy as np
+    n = len(model.X)
+    k = model._size_transformed()
+    l_hat = model.log_likelihood()
+    return np.log(n) * k - 2 * np.log(l_hat)
