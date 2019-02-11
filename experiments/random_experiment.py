9487703632cf5ea3af8c09f351eebe047b467a65
Louis Schlessinger <louschlessinger@gmail.com>
2019-02-11 17:53:42 -0500

    Fix random search strategy experiment

--- experiments/random_experiment.py
+++ experiments/random_experiment.py
@@ -1,9 +1,5 @@
-import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
-from sklearn.svm import SVR
 
 from autoks import model
 from autoks.experiment import Experiment
@@ -34,43 +30,6 @@ objective = negative_BIC
 # use conjugate gradient descent
 optimizer = 'scg'
 
-experiment = Experiment(grammar, objective, base_kernels, X_train, y_train.reshape(-1, 1), eval_budget=50, debug=True,
+experiment = Experiment(grammar, objective, base_kernels, X_train, y_train, X_test, y_test, eval_budget=50, debug=True,
                         verbose=True, optimizer=optimizer)
-aks_kernels = experiment.kernel_search()
-
-# View results of experiment
-experiment.plot_best_scores()
-experiment.plot_score_summary()
-
-sorted_aks_kernels = sorted(aks_kernels, key=lambda x: x.score, reverse=True)
-best_aks_kernel = sorted_aks_kernels[0]
-best_kernel = best_aks_kernel.kernel
-print('Best kernel:')
-best_aks_kernel.pretty_print()
-
-best_model = experiment.gp_model.__class__(experiment.X, experiment.y, kernel=best_kernel)
-
-# If training data is 1D, show a plot.
-if best_model.input_dim == 1:
-    best_model.plot(plot_density=True)
-    plt.show()
-
-mean, var = best_model.predict(X_test)
-y_pred = mean
-rmse = np.sqrt(mean_squared_error(y_test, y_pred))
-print('RMSE = %.3f' % rmse)
-
-mean_lpd = np.mean(best_model.log_predictive_density(X_test, y_test))
-print('Negative log predictive density = %.3f' % -mean_lpd)
-
-# Compare against linear and support vector regression.
-lin_reg = LinearRegression().fit(X_train, y_train)
-y_pred_lin_reg = lin_reg.predict(X_test)
-rmse_lin_reg = np.sqrt(mean_squared_error(y_test, y_pred_lin_reg))
-
-svr = SVR().fit(X_train, y_train)
-y_pred_svr = svr.predict(X_test)
-rmse_svr = np.sqrt(mean_squared_error(y_test, y_pred_svr))
-
-print('RMSE Linear Regression = %.3f' % rmse_lin_reg)
-print('RMSE SVM = %.3f' % rmse_svr)
+experiment.run()
