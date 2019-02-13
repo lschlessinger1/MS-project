aafc7fcfa810120ceccb82bd5e9d70cf12499840
Lou <louschlessinger96@gmail.com>
2019-02-13 12:41:56 -0500

    Fix imports and remove unused init files

--- experiments/random_experiment.py
+++ experiments/random_experiment.py
@@ -1,5 +1,12 @@
 import numpy as np
 from sklearn.model_selection import train_test_split
+import os
+import sys
+
+top_path = os.path.abspath('..')
+if top_path not in sys.path:
+    print('Adding to sys.path %s' % top_path)
+    sys.path.append(top_path)
 
 from autoks import model
 from autoks.experiment import Experiment
