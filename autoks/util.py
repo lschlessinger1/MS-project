43c091ce72c832dfc46ac92f6d8f036cc9ac5d8e
Louis Schlessinger <louschlessinger@gmail.com>
2019-02-19 17:14:19 -0500

    Add type hinting to autoks util

--- autoks/util.py
+++ autoks/util.py
@@ -1,9 +1,17 @@
-def argsort(unsorted_list):
+def argsort(unsorted_list: list):
+    """Argument-sort a list
+
+    :param unsorted_list: a list of unsorted items
+    :return:
+    """
     return [i[0] for i in sorted(enumerate(unsorted_list), key=lambda x: x[1])]
 
 
-def argunique(data):
-    """ Get the indices of the unique elements in a list
+def argunique(data: list):
+    """Get the indices of the unique elements in a list.
+
+    :param data:
+    :return:
     """
     unique_vals = set()
     unique_ind = []
@@ -16,8 +24,12 @@ def argunique(data):
     return unique_ind
 
 
-def remove_duplicates(data, values):
-    """ Remove duplicates of data and replace with values
+def remove_duplicates(data: list, values: list):
+    """Remove duplicates of data and replace with values.
+
+    :param data:
+    :param values:
+    :return:
     """
     if len(data) != len(values):
         raise ValueError('The length of data must be be equal to the length of values')
