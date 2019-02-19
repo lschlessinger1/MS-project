c4c6e7bf02e1d639a3eab81cf625481fb44a5273
Louis Schlessinger <louschlessinger@gmail.com>
2019-02-19 15:54:02 -0500

    Add type hinting to genprog

--- evalg/genprog.py
+++ evalg/genprog.py
@@ -1,5 +1,6 @@
 import copy
 from abc import ABC
+from typing import List
 
 import numpy as np
 
@@ -10,7 +11,7 @@ from evalg.mutation import Mutator
 
 class BinaryTreeGenerator:
 
-    def __init__(self, binary_operators, operands, max_depth):
+    def __init__(self, binary_operators: List[str], operands, max_depth: int):
         self.binary_operators = binary_operators
         self.operands = operands
         if max_depth < 0:
@@ -29,7 +30,7 @@ class BinaryTreeGenerator:
 
 class GrowGenerator(BinaryTreeGenerator):
 
-    def __init__(self, binary_operators, operands, max_depth):
+    def __init__(self, binary_operators: List[str], operands, max_depth: int):
         super().__init__(binary_operators, operands, max_depth)
 
     def generate(self):
@@ -39,7 +40,7 @@ class GrowGenerator(BinaryTreeGenerator):
         """
         return BinaryTree(self.grow(depth=0))
 
-    def grow(self, depth):
+    def grow(self, depth: int):
         """Grow a random binary tree node
 
         :param depth: the level of the current tree
@@ -76,13 +77,13 @@ class GrowGenerator(BinaryTreeGenerator):
 
 class FullGenerator(BinaryTreeGenerator):
 
-    def __init__(self, binary_operators, operands, max_depth):
+    def __init__(self, binary_operators: List[str], operands, max_depth: int):
         super().__init__(binary_operators, operands, max_depth)
 
     def generate(self):
         return BinaryTree(self.full(depth=0))
 
-    def full(self, depth):
+    def full(self, depth: int):
         """ Grow a random tree"""
         if depth < 0:
             raise ValueError('depth must be nonnegative.')
@@ -138,14 +139,6 @@ def check_binary_trees(f):
     return wrapper
 
 
-#
-# class TreeRecombinator(Recombinator, ABC):
-#
-#     @check_binary_trees
-#     def crossover(self, parents):
-#         raise NotImplementedError('crossover must be implemented in a child class')
-
-
 # Binary tree mutators
 
 class TreePointMutator(TreeMutator):
@@ -180,7 +173,7 @@ class TreePointMutator(TreeMutator):
 
 class SubTreeExchangeMutator(TreeMutator, ABC):
 
-    def __init__(self, operands, max_depth):
+    def __init__(self, operands, max_depth: int):
         """
 
         :param operands:
@@ -192,7 +185,7 @@ class SubTreeExchangeMutator(TreeMutator, ABC):
         self.max_depth = max_depth
 
     @staticmethod
-    def _mutate_subtree_exchange(tree, tree_generator):
+    def _mutate_subtree_exchange(tree: BinaryTree, tree_generator: BinaryTreeGenerator):
         """
 
         :param tree:
@@ -209,7 +202,7 @@ class SubTreeExchangeMutator(TreeMutator, ABC):
         return new_tree
 
     @staticmethod
-    def _swap_mut_subtree(tree, r, random_tree):
+    def _swap_mut_subtree(tree: BinaryTree, r: int, random_tree: BinaryTree):
         """ Add mutated subtree to original tree
 
         :param tree:
@@ -237,7 +230,7 @@ class SubTreeExchangeMutator(TreeMutator, ABC):
 
 class GrowMutator(SubTreeExchangeMutator):
 
-    def __init__(self, operands, max_depth=2):
+    def __init__(self, operands, max_depth: int = 2):
         super().__init__(operands, max_depth)
 
     def mutate(self, individual: BinaryTree):
@@ -252,7 +245,7 @@ class GrowMutator(SubTreeExchangeMutator):
 
 class HalfAndHalfMutator(SubTreeExchangeMutator):
 
-    def __init__(self, operands, max_depth=2):
+    def __init__(self, operands, max_depth: int = 2):
         super().__init__(operands, max_depth)
 
     def mutate(self, individual: BinaryTree):
@@ -271,7 +264,7 @@ class SubtreeExchangeBinaryRecombinator(Recombinator):
 
     @check_binary_trees
     @check_two_parents
-    def crossover(self, parents):
+    def crossover(self, parents: List[BinaryTree]):
         """Sub-tree exchange crossover
 
         :return:
@@ -293,7 +286,7 @@ class SubtreeExchangeBinaryRecombinator(Recombinator):
         return tree_1, tree_2
 
     @staticmethod
-    def _swap_subtrees(node_1, node_2):
+    def _swap_subtrees(node_1: BinaryTreeNode, node_2: BinaryTreeNode):
         """Swap parents and of nodes
         """
 
@@ -322,7 +315,7 @@ class SubtreeExchangeBinaryRecombinator(Recombinator):
         return node_1, node_2
 
     @staticmethod
-    def _valid_pair(postfix_token_1, postfix_token_2):
+    def _valid_pair(postfix_token_1: str, postfix_token_2: str):
         """Checks if postfix token pair is valid
 
         :param postfix_token_1: The first token in post-order notation
@@ -337,7 +330,7 @@ class SubtreeExchangeBinaryRecombinator(Recombinator):
         return False
 
     @staticmethod
-    def _select_token_ind(postfix_tokens_1, postfix_tokens_2):
+    def _select_token_ind(postfix_tokens_1: str, postfix_tokens_2: str):
         """Select indices of parent postfix tokens
 
         :param postfix_tokens_1: The first list of tokens in post-order notation
