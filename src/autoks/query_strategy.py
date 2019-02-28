a3feb03ba1efa6aa463d61374390aa54e84b148c
Louis Schlessinger <louschlessinger@gmail.com>
2019-02-28 11:58:20 -0600

    Add partial implementation of query strategy

---
+++ src/autoks/query_strategy.py
@@ -0,0 +1,76 @@
+from typing import Callable, List
+
+import numpy as np
+
+from src.autoks.kernel import AKSKernel
+
+
+# Query Strategies
+
+class QueryStrategy:
+    """Propose a model to evaluate
+
+    Given a list of un-evaluated models, a scoring function, and training data
+    we return x_star (chosen)
+    """
+
+    def __init__(self, scoring_func: Callable):
+        self.scoring_func = scoring_func
+
+    def select(self, kernels: List[AKSKernel], X_train: np.array, y_train: np.array):
+        raise NotImplementedError('Method must be implemented in a child class')
+
+
+class NaiveQueryStrategy(QueryStrategy):
+
+    def __init__(self, scoring_func: Callable = None):
+        if scoring_func is None:
+            scoring_func = score_all_same
+        super().__init__(scoring_func)
+
+    def select(self, kernels: List[AKSKernel], X_train: np.array, y_train: np.array):
+        """Select all kernels
+
+        :param kernels:
+        :param X_train:
+        :param y_train:
+        :return:
+        """
+        scores = [self.scoring_func(kernel) for kernel in kernels]
+        return kernels, scores
+
+
+class BestScoreStrategy(QueryStrategy):
+
+    def __init__(self, scoring_func: Callable):
+        super().__init__(scoring_func)
+
+    def select(self, kernels: List[AKSKernel], X_train: np.array, y_train: np.array):
+        """Select best kernel according to scoring func
+
+        :param kernels:
+        :param X_train:
+        :param y_train:
+        :return:
+        """
+        scores = [self.scoring_func(kernel) for kernel in kernels]
+        x_star = kernels[int(np.argmax(scores))]
+        return x_star, scores
+
+
+# Scoring Functions
+
+def score_all_same(kernel: AKSKernel):
+    return 1
+
+
+def expected_improvement(kernel: AKSKernel, hyperpriors, X_train: np.array, y_train: np.array):
+    """Expected improvement (EI) acquisition function
+
+    This acquisition function takes a model (kernel and hyperpriors) and computes expected improvement using
+    the posterior Hellinger squared exponential covariance between models conditioned on the training data
+
+    :param kernel:
+    :return:
+    """
+    pass
