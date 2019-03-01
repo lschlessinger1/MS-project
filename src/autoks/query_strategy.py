0a07e47cabe08b27e7e40584cb6d59217065b157
Louis Schlessinger <louschlessinger@gmail.com>
2019-02-28 22:38:25 -0600

    Clean up query strategy type hinting

--- src/autoks/query_strategy.py
+++ src/autoks/query_strategy.py
@@ -12,12 +12,28 @@ from src.evalg.selection import Selector, AllSelector
 class AcquisitionFunction:
 
     def score(self, kernel: AKSKernel, X_train: np.array, y_train: np.array, hyperpriors=None) -> int:
+        """Acquisition function score.
+
+        :param kernel:
+        :param X_train:
+        :param y_train:
+        :param hyperpriors:
+        :return:
+        """
         raise NotImplementedError('Must be implemented in a child class')
 
 
 class UniformScorer(AcquisitionFunction):
 
     def score(self, kernel: AKSKernel, X_train: np.array, y_train: np.array, hyperpriors=None) -> int:
+        """Same score for all kernels.
+
+        :param kernel:
+        :param X_train:
+        :param y_train:
+        :param hyperpriors:
+        :return:
+        """
         return 1
 
 
@@ -26,8 +42,8 @@ class ExpectedImprovement(AcquisitionFunction):
     def score(self, kernel: AKSKernel, X_train: np.array, y_train: np.array, hyperpriors=None) -> int:
         """Expected improvement (EI) acquisition function
 
-    This acquisition function takes a model (kernel and hyperpriors) and computes expected improvement using
-    the posterior Hellinger squared exponential covariance between models conditioned on the training data
+        This acquisition function takes a model (kernel and hyperpriors) and computes expected improvement using
+        the posterior Hellinger squared exponential covariance between models conditioned on the training data
 
         :param kernel:
         :param X_train:
@@ -53,12 +69,28 @@ class QueryStrategy(Selector, ABC):
 
     def query(self, kernels: List[AKSKernel], X_train: np.array, y_train: np.array, hyperpriors=None) -> \
             Tuple[np.array, List[float]]:
+        """Query the next round of kernels using the acquisition function.
+
+        :param kernels:
+        :param X_train:
+        :param y_train:
+        :param hyperpriors:
+        :return:
+        """
         scores = self.score_kernels(kernels, X_train, y_train, hyperpriors)
         ind = self.arg_select(np.array(kernels), scores)
         return ind, scores
 
     def score_kernels(self, kernels: List[AKSKernel], X_train: np.array, y_train: np.array, hyperpriors=None) -> \
             List[float]:
+        """Score all kernels using the scoring function.
+
+        :param kernels:
+        :param X_train:
+        :param y_train:
+        :param hyperpriors:
+        :return:
+        """
         return [self.scoring_func.score(kernel, X_train, y_train, hyperpriors) for kernel in kernels]
 
 
@@ -76,10 +108,16 @@ class BestScoreStrategy(QueryStrategy):
         super().__init__(n_individuals, scoring_func)
 
     def select(self, population: np.array, scores: np.array) -> list:
+        """See parent docstring.
+
+        :param population:
+        :param scores:
+        :return:
+        """
         return self._select_helper(population, scores)
 
     def arg_select(self, population: np.array, scores: np.array) -> List[int]:
-        """Select best kernel according to scoring function
+        """Select best kernel according to scoring function.
 
         :param population:
         :param scores:
