7785fedead6cb263855e5e110b12cebd045f7cd5
Louis Schlessinger <louschlessinger@gmail.com>
2019-03-01 01:46:02 -0600

    Clean up evalg util type hinting and docstring

--- src/evalg/util.py
+++ src/evalg/util.py
@@ -1,4 +1,10 @@
-def swap(arr: list, i: int, j: int):
-    """Swap two array elements."""
+def swap(arr: list, i: int, j: int) -> list:
+    """Swap two array elements.
+
+    :param arr:
+    :param i:
+    :param j:
+    :return:
+    """
     arr[i], arr[j] = arr[j], arr[i]
     return arr
