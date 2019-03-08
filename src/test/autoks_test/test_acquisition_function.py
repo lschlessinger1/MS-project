3fe298c172194d2f9731f55f63687dd4dbcc11e1
Louis Schlessinger <louschlessinger@gmail.com>
2019-03-07 22:29:42 -0500

    Add acquisition function tests

---
+++ src/test/autoks_test/test_acquisition_function.py
@@ -0,0 +1,29 @@
+from unittest import TestCase
+
+import numpy as np
+from GPy.kern import RBF
+
+from src.autoks.acquisition_function import AcquisitionFunction, UniformScorer
+from src.autoks.kernel import AKSKernel
+
+
+class TestAcquisitionFunction(TestCase):
+
+    def test_score(self):
+        f_acq = AcquisitionFunction()
+        kernel = AKSKernel(RBF(1))
+        x_train = np.array([[1, 2, 3], [4, 5, 6]])
+        y_train = np.array([[5], [10]])
+        self.assertRaises(NotImplementedError, f_acq.score, kernel, x_train, y_train)
+
+
+class TestUniformScorer(TestCase):
+
+    def test_score(self):
+        f_acq = UniformScorer()
+        kernel = AKSKernel(RBF(1))
+        x_train = np.array([[1, 2, 3], [4, 5, 6]])
+        y_train = np.array([[5], [10]])
+        result = f_acq.score(kernel, x_train, y_train)
+        self.assertIsInstance(result, float)
+        self.assertEqual(result, UniformScorer.CONST_SCORE)
