27cc8140824e5b24d695b3282f3d67cf85f86fb7
Louis Schlessinger <louschlessinger@gmail.com>
2019-03-14 11:44:33 -0400

    add boms known kernel experiment

---
+++ src/experiments/boms/boms_synthetic_known_kern_exp.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+from src.autoks.grammar import BOMSGrammar
+from src.autoks.kernel_selection import BOMS_kernel_selector
+from src.autoks.model import log_likelihood_normalized
+from src.experiments.util.data_util import KnownGPDataset, cks_known_kernels, run_experiments
+
+# Set random seed for reproducibility.
+np.random.seed(4096)
+
+grammar = BOMSGrammar()
+kernel_selector = BOMS_kernel_selector()
+objective = log_likelihood_normalized
+
+# Create synthetic dataset generators
+noise_vars = [10 ** i for i in range(-1, 2)]
+datasets = [KnownGPDataset(kernel, var, 100) for var in noise_vars for kernel in cks_known_kernels()]
+
+run_experiments(datasets, grammar, kernel_selector, objective, base_kernels=None, eval_budget=50,
+                debug=True, verbose=True)
