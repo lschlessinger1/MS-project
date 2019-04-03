188b76c71a2a97de612115762522ae5ae583ad09
Louis Schlessinger <louschlessinger@gmail.com>
2019-04-03 17:57:21 -0400

    Clean up node check test case

--- src/test/evalg/support/util.py
+++ src/test/evalg/support/util.py
@@ -1,9 +1,19 @@
+from typing import Optional, TypeVar
 from unittest import TestCase
+
+from src.evalg.encoding import BinaryTreeNode
 
 
 class NodeCheckTestCase(TestCase):
+    """Base test case class used for checking binary tree nodes"""
+    T = TypeVar('T')
 
-    def _check_node(self, node, value, left_value, right_value, parent_value):
+    def _check_node(self,
+                    node: BinaryTreeNode,
+                    value: Optional[T],
+                    left_value: Optional[T],
+                    right_value: Optional[T],
+                    parent_value: Optional[T]) -> None:
         self.assertEqual(value, node.value, 'Node values not equal')
         if not node.left:
             self.assertIsNone(left_value, 'Left value not none')
@@ -20,11 +30,20 @@ class NodeCheckTestCase(TestCase):
         else:
             self.assertEqual(parent_value, node.parent.value, 'Parent values not equal')
 
-    def check_root(self, parent_node, value, left_value, right_value):
+    def check_root(self,
+                   parent_node: BinaryTreeNode,
+                   value: T,
+                   left_value: T,
+                   right_value: T) -> None:
         self._check_node(parent_node, value, left_value, right_value, None)
 
-    def check_leaf(self, leaf_node, value, parent_value):
+    def check_leaf(self,
+                   leaf_node: BinaryTreeNode,
+                   value: T,
+                   parent_value: T) -> None:
         self._check_node(leaf_node, value, None, None, parent_value)
 
-    def check_stump(self, stump_node, value):
+    def check_stump(self,
+                    stump_node: BinaryTreeNode,
+                    value: T) -> None:
         self._check_node(stump_node, value, None, None, None)
