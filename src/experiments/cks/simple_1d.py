cdd49f5912fc8acbb1497116bf5a818359ea485f
Lou <louschlessinger96@gmail.com>
2019-04-10 15:12:38 -0400

    Add kwargs to simple experiments

--- src/experiments/cks/simple_1d.py
+++ src/experiments/cks/simple_1d.py
@@ -2,5 +2,5 @@ from src.autoks.experiment import Experiment
 from src.experiments.util.synthetic_data import Sinosoid1Dataset
 
 dataset = Sinosoid1Dataset(input_dim=1)
-experiment = Experiment.cks_experiment(dataset)
+experiment = Experiment.cks_experiment(dataset, verbose=True, debug=True)
 experiment.run(title='Simple CKS Experiment')
--- src/experiments/evolutionary/simple_1d.py
+++ src/experiments/evolutionary/simple_1d.py
@@ -2,5 +2,5 @@ from src.autoks.experiment import Experiment
 from src.experiments.util.synthetic_data import Sinosoid1Dataset
 
 dataset = Sinosoid1Dataset(input_dim=1)
-experiment = Experiment.evolutionary_experiment(dataset)
+experiment = Experiment.evolutionary_experiment(dataset, verbose=True, debug=True)
 experiment.run(title='Simple Evolutionary Experiment')
