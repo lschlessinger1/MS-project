3e00b492a8870abbabcea3da7a8d5fa42a7d4b5f
Louis Schlessinger <louschlessinger@gmail.com>
2019-04-10 13:37:23 -0400

    Fix imports

--- src/experiments/boms/all_files.py
+++ src/experiments/boms/all_files.py
@@ -1,5 +1,5 @@
 from src.autoks.experiment import Experiment
-from src.experiments.util.data_util import FileExperimentRunner
+from src.experiments.util.experiment_runner import FileExperimentRunner
 
 exp_runner = FileExperimentRunner(Experiment.boms_experiment)
 exp_runner.run(additive_form=False, verbose=True, debug=True)
