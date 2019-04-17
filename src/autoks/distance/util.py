589beedc8a6d9edd1988a272d7cd17f95392966c
Lou <louschlessinger96@gmail.com>
2019-04-17 16:35:06 -0400

    Add prior sample

--- src/autoks/distance/util.py
+++ src/autoks/distance/util.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.stats import norm
 
 from src.autoks.distance.sobol import sobol_sample
 
@@ -16,3 +17,23 @@ def probability_samples(max_num_hyperparameters: int = 40,
     #  - leap is causing sample to be very slow (therefore, set it to 0 instead of 100)
     #  - scramble should be the Matouse-Affine-Owen scramble method
     return sobol_sample(num_samples, max_num_hyperparameters, skip=1000, leap=0, scramble=True)
+
+
+def prior_sample(priors: np.ndarray,
+                 prob_samples: np.ndarray) -> np.ndarray:
+    """
+    Return num_samples x priors.shape[0] array
+    """
+    num_samples = prob_samples.shape[0]
+    num_hyp = priors.size
+    prior_mean = np.zeros((1, priors.size))
+    prior_std = np.zeros((1, priors.size))
+    for i in range(num_hyp):
+        prior = priors[i]
+        prior_mean[0, i] = prior.mu
+        prior_std[0, i] = prior.sigma
+
+    hyps = norm.ppf(prob_samples[:, :num_hyp],
+                    np.tile(prior_mean, (num_samples, 1)),
+                    np.tile(prior_std, (num_samples, 1)))
+    return hyps
--- src/test/autoks/test_distance_util.py
+++ src/test/autoks/test_distance_util.py
@@ -1,8 +1,9 @@
 from unittest import TestCase
 
 import numpy as np
+from GPy.core.parameterization.priors import Gaussian
 
-from src.autoks.distance.util import probability_samples
+from src.autoks.distance.util import probability_samples, prior_sample
 
 
 class TestDistanceUtil(TestCase):
@@ -13,3 +14,25 @@ class TestDistanceUtil(TestCase):
         result = probability_samples(max_num_hyperparameters=m, num_samples=n)
         assert isinstance(result, np.ndarray)
         assert result.shape == (n, m)
+
+    def test_prior_sample_fast(self):
+        prior_1 = Gaussian(mu=0.5, sigma=2)
+        prior_2 = Gaussian(mu=0.7, sigma=2.2)
+        prior_list = np.array([prior_1, prior_2])
+
+        samples = probability_samples()
+        result = prior_sample(prior_list, samples)
+        self.assertIsInstance(result, np.ndarray)
+        self.assertEqual(result.shape, (samples.shape[0], prior_list.size))
+
+    def test_prior_sample_slow(self):
+        prior_1 = Gaussian(mu=0.5, sigma=2)
+        prior_2 = Gaussian(mu=0.7, sigma=2.2)
+        prior_list = np.array([prior_1, prior_2])
+        samples = probability_samples(num_samples=2000)
+        prior_samp = prior_sample(prior_list, samples)
+        # Test mean and standard deviation are within 0.1 error from expected
+        np.testing.assert_approx_equal(prior_samp[:, 0].std(), prior_list[0].sigma, significant=1)
+        np.testing.assert_approx_equal(prior_samp[:, 0].mean(), prior_list[0].mu, significant=1)
+        np.testing.assert_approx_equal(prior_samp[:, 1].std(), prior_list[1].sigma, significant=1)
+        np.testing.assert_approx_equal(prior_samp[:, 1].mean(), prior_list[1].mu, significant=1)
