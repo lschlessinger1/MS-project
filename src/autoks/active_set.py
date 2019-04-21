fad50ebcb747387c649fa45422825f15225792d8
Louis Schlessinger <louschlessinger@gmail.com>
2019-04-21 18:01:39 -0400

    Add active set

---
+++ src/autoks/active_set.py
@@ -0,0 +1,78 @@
+from typing import List, Optional, Tuple
+
+from src.autoks.kernel import AKSKernel
+
+# We define a model to be this for the active set
+Model = AKSKernel
+
+
+class ActiveSet:
+    _remove_priority: List[int]
+    _models: List[Optional[Model]]
+
+    def __init__(self, max_n_models: int):
+        self.max_n_models = max_n_models
+        self._models = [None] * self.max_n_models
+        self._remove_priority = []  # list of indices representing order in which to remove models
+
+    @property
+    def remove_priority(self):
+        return self._remove_priority
+
+    @remove_priority.setter
+    def remove_priority(self, remove_priority):
+        if len(remove_priority) > self.max_n_models:
+            raise ValueError(f'Size of removal priority {len(remove_priority)} cannot be greater than maximum number '
+                             f'of models {self.max_n_models}.')
+
+        # also check for improper indices
+        if min(remove_priority) < 0 or self.max_n_models - 1 < max(remove_priority):
+            raise ValueError(f'All indices must be between 0 <= i <= {self.max_n_models - 1}.')
+
+        self._remove_priority = remove_priority.copy()
+
+    @property
+    def models(self) -> List[Optional[Model]]:
+        return self._models
+
+    @models.setter
+    def models(self, models):
+        if len(models) > self.max_n_models:
+            raise ValueError(f'Size of models {len(models)} cannot be greater than maximum number of models '
+                             f'{self.max_n_models}.')
+        self._models = models
+
+    def get_index_to_insert(self):
+        n_models = len(self)
+        if n_models < self.max_n_models:
+            return n_models
+
+        if len(self.remove_priority) == 0:
+            raise ValueError(f'Must set removal priority when {self.__class__.__name__} is full.')
+
+        return self.remove_priority.pop(0)
+
+    def update(self, candidates: List[Model]) -> List[int]:
+        """Returns new candidate indices"""
+        newly_inserted_indices = []
+
+        for candidate in candidates:
+            inserted_index, success = self.add_model(candidate)
+            if success:
+                newly_inserted_indices.append(inserted_index)
+
+        # Assume that any model that exceeded the size limit was replaced.
+        return newly_inserted_indices[-self.max_n_models:]
+
+    def add_model(self, model: Model) -> Tuple[int, bool]:
+        if model in self.models:
+            return -1, False
+
+        idx = self.get_index_to_insert()
+
+        self._models[idx] = model
+
+        return idx, True
+
+    def __len__(self):
+        return sum(1 for m in self.models if m is not None)
---
+++ src/test/autoks/test_active_set.py
@@ -0,0 +1,126 @@
+from unittest import TestCase
+
+from GPy.kern import RBF, RationalQuadratic
+
+from src.autoks.active_set import ActiveSet
+from src.autoks.kernel import AKSKernel
+
+
+class TestActiveSet(TestCase):
+
+    def setUp(self) -> None:
+        self.active_set = ActiveSet(max_n_models=5)
+
+    def test_set_bad_remove_priority(self):
+        with self.assertRaises(ValueError):
+            remove_priority = [0, 1, 2, 3, 4, 0]
+            self.active_set.remove_priority = remove_priority
+        with self.assertRaises(ValueError):
+            remove_priority = [10]
+            self.active_set.remove_priority = remove_priority
+
+    def test_get_index_to_insert_empty(self):
+        expected_index = 0
+        actual_index = self.active_set.get_index_to_insert()
+        self.assertEqual(expected_index, actual_index)
+
+    def test_get_index_to_insert_one_item(self):
+        # add one model
+        self.active_set.add_model(AKSKernel(RBF(1)))
+
+        expected_index = 1
+        actual_index = self.active_set.get_index_to_insert()
+        self.assertEqual(expected_index, actual_index)
+
+    def test_get_index_to_insert_full_no_priority(self):
+        # add five models
+        self.active_set.add_model(AKSKernel(RBF(1)))
+        self.active_set.add_model(AKSKernel(RationalQuadratic(1)))
+        self.active_set.add_model(AKSKernel(RBF(1)))
+        self.active_set.add_model(AKSKernel(RationalQuadratic(1)))
+        self.active_set.add_model(AKSKernel(RBF(1)))
+
+        self.assertRaises(ValueError, self.active_set.get_index_to_insert)
+
+    def test_get_index_to_insert_full_with_priority(self):
+        # add five models
+        self.active_set.add_model(AKSKernel(RBF(1)))
+        self.active_set.add_model(AKSKernel(RationalQuadratic(1)))
+        self.active_set.add_model(AKSKernel(RBF(1)))
+        self.active_set.add_model(AKSKernel(RationalQuadratic(1)))
+        self.active_set.add_model(AKSKernel(RBF(1)))
+
+        remove_priority = [2]
+        self.active_set.remove_priority = remove_priority
+        actual = self.active_set.get_index_to_insert()
+        expected = 2
+        self.assertEqual(expected, actual)
+        self.assertEqual(self.active_set.remove_priority, [])
+
+        remove_priority = [0, 2, 3]
+        self.active_set.remove_priority = remove_priority
+        actual = self.active_set.get_index_to_insert()
+        expected = 0
+        self.assertEqual(expected, actual)
+        self.assertEqual(self.active_set.remove_priority, [2, 3])
+
+    def test_add_model_empty(self):
+        candidate = AKSKernel(RBF(1))
+        expected_ind, expected_status = 0, True
+        actual_ind, actual_status = self.active_set.add_model(candidate)
+        self.assertEqual(expected_ind, actual_ind)
+        self.assertEqual(expected_status, actual_status)
+
+        expected_models = [candidate, None, None, None, None]
+        self.assertListEqual(expected_models, self.active_set.models)
+
+        expected_next_ind = 1
+        self.assertEqual(expected_next_ind, self.active_set.get_index_to_insert())
+
+    def test_add_model_same(self):
+        # TODO: should this actually be same kernel?
+        models = [AKSKernel(RBF(1))] * 2
+        self.active_set.add_model(models[0])
+
+        expected_ind = -1
+        expected_status = False
+        actual_ind, actual_status = self.active_set.add_model(models[1])
+        self.assertEqual(expected_ind, actual_ind)
+        self.assertEqual(expected_status, actual_status)
+
+        expected_models = [models[0], None, None, None, None]
+        self.assertListEqual(expected_models, self.active_set.models)
+
+        expected_next_ind = 1
+        self.assertEqual(expected_next_ind, self.active_set.get_index_to_insert())
+
+    def test_update_empty(self):
+        candidates = [AKSKernel(RBF(1)), AKSKernel(RationalQuadratic(1))]
+        expected_candidates_ind = [0, 1]
+        new_candidates_ind = self.active_set.update(candidates)
+        self.assertEqual(expected_candidates_ind, new_candidates_ind)
+
+        expected_models = [candidates[0], candidates[1], None, None, None]
+        self.assertListEqual(expected_models, self.active_set.models)
+
+        expected_next_ind = 2
+        self.assertEqual(expected_next_ind, self.active_set.get_index_to_insert())
+
+    def test_update_exceed_max_no_remove(self):
+        candidates = [AKSKernel(RBF(1)), AKSKernel(RBF(1)), AKSKernel(RBF(1)), AKSKernel(RBF(1)), AKSKernel(RBF(1)),
+                      AKSKernel(RBF(1))]
+        self.assertRaises(ValueError, self.active_set.update, candidates)
+
+    def test_update_exceed_max_remove_set(self):
+        candidates = [AKSKernel(RBF(1)), AKSKernel(RBF(1)), AKSKernel(RBF(1)), AKSKernel(RBF(1)), AKSKernel(RBF(1)),
+                      AKSKernel(RationalQuadratic(1))]
+        self.active_set.remove_priority = [0, 1, 2, 3, 4]
+        actual_new_candidates_ind = self.active_set.update(candidates)
+        expected_new_candidates_ind = [1, 2, 3, 4, 0]
+        self.assertListEqual(expected_new_candidates_ind, actual_new_candidates_ind)
+
+        expected_models = [candidates[5], candidates[1], candidates[2], candidates[3], candidates[4]]
+        self.assertListEqual(expected_models, self.active_set.models)
+
+        expected_next_ind = 1
+        self.assertEqual(expected_next_ind, self.active_set.get_index_to_insert())
