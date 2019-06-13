49b149cb0f65031ea444c7deeb996542e286dd8e
Louis Schlessinger <louschlessinger@gmail.com>
2019-06-13 12:29:41 -0400

    Add dataset init file

---
+++ src/datasets/__init__.py
@@ -0,0 +1,6 @@
+from .airline import AirlineDataset
+from .concrete import ConcreteDataset
+from .housing import HousingDataset
+from .mauna import MaunaDataset
+from .methane import MethaneDataset
+from .solar import SolarDataset
