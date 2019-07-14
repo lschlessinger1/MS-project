f34e8510ae195a6f3473eb3041c20d42c728f622
Louis Schlessinger <louschlessinger@gmail.com>
2019-07-13 19:05:11 -0500

    Remove known gp dataset experiments

--- src/training/experiments/random/known_gp.py
+++
@@ -1,5 +0,0 @@
-from src.autoks.core.model_search_experiment import ModelSearchExperiment
-from src.training.experiments.experiment_runner import KnownGPExperimentRunner
-
-exp_runner = KnownGPExperimentRunner(ModelSearchExperiment.random_experiment)
-exp_runner.run(additive_form=False)
