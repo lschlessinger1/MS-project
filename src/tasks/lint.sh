fde47b6f53502907eceb3bb2df2334985c9af49f
Louis Schlessinger <louschlessinger@gmail.com>
2019-07-25 21:56:09 -0400

    Add bandit

--- src/tasks/lint.sh
+++ src/tasks/lint.sh
@@ -7,6 +7,9 @@ FAILURE=false
 echo "pipenv check"
 pipenv check  # Not reporting failure here, because sometimes this fails due to API request limit
 
+echo "bandit"
+pipenv run bandit -ll -r {autoks,evalg,datasets,training} -x demos,playground || FAILURE=true
+
 echo "shellcheck"
 shellcheck tasks/*.sh || FAILURE=true
 
