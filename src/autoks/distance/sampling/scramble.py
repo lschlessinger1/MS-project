a2f4716ea87f699e0c7532a640c2ec36e8700f46
Lou Schlessinger <louschlessinger96@gmail.com>
2019-10-27 13:11:34 -0500

    Fix scrambling

--- src/autoks/distance/sampling/scramble.py
+++ src/autoks/distance/sampling/scramble.py
@@ -2,9 +2,10 @@ import numpy as np
 
 
 def scramble_array(a: np.ndarray) -> None:
-    """In-place scrambling of an array."""
+    """In-place scrambling of an array along first dimension."""
     # FIXME: This should actually use the Matousek-Affine-Owen scrambling algorithm
     # Matousek, J. “On the L2-Discrepancy for Anchored Boxes.” Journal of Complexity.
     # Vol. 14, Number 4, 1998, pp. 527–556.
     # for now, use a Naive scrambling method
-    np.random.shuffle(a)
+    for i in range(a.shape[1]):
+        np.random.shuffle(a[:, i])
