8a6ea4c79acec1774841250c46df34d83bb59524
Lou Schlessinger <louschlessinger96@gmail.com>
2019-10-28 19:16:57 -0500

    Refactor SMBO model selector

--- src/autoks/core/model_selection/smbo_model_selector.py
+++ src/autoks/core/model_selection/smbo_model_selector.py
@@ -1,17 +1,17 @@
-import warnings
 from typing import List, Union, Callable, Optional
 
-import numpy as np
-
+from src.autoks.acquisition import expected_improvement
 from src.autoks.backend.model import RawGPModelType
 from src.autoks.callbacks import CallbackList
 from src.autoks.core.covariance import Covariance
 from src.autoks.core.gp_model import GPModel, pretty_print_gp_models
 from src.autoks.core.gp_model_population import GPModelPopulation, ActiveModelPopulation
 from src.autoks.core.grammar import BaseGrammar
-from src.autoks.core.model_selection import SurrogateEvolutionaryModelSelector
+from src.autoks.core.hyperprior import boms_hyperpriors
 from src.autoks.core.model_selection.base import ModelSelector
-from src.autoks.distance.distance import ActiveModels
+from src.autoks.core.prior import PriorDist
+from src.autoks.core.strategies.bayes_opt_gp_strategy import BayesOptGPStrategy
+from src.autoks.distance.distance import ActiveModels, CorrelationDistanceBuilder
 
 
 class SMBOModelSelector(ModelSelector):
@@ -25,14 +25,20 @@ class SMBOModelSelector(ModelSelector):
                  **ms_args):
         self.eval_budget_low_level = ms_args.pop("eval_budget_low_level", 100)
         base_kernel_names = ms_args.get('base_kernel_names', None)
-        grammar = BaseGrammar(base_kernel_names=base_kernel_names)
+        grammar = BaseGrammar(base_kernel_names=base_kernel_names, hyperpriors=boms_hyperpriors())
         super().__init__(grammar, fitness_fn, gp_fn=gp_fn, gp_args=gp_args)
         self.gp_fn = gp_fn
         self.gp_args = gp_args
         self.model_selector_args = ms_args
 
         self.active_models = None
-        self.covariance_to_info_map = dict()
+        self.acquisition_fn = expected_improvement
+        self.max_n_models = 1000
+        self.num_samples = 20
+        self.max_n_hyperparameters = 100
+        noise_prior_args = self.grammar.hyperpriors['GP']['variance']._raw_prior_args
+        self.noise_prior = PriorDist.from_prior_str("GAUSSIAN", noise_prior_args).raw_prior
+        # TODO: include strategy and distance builder string here
 
     def _train(self, eval_budget: int, max_generations: int, callbacks: CallbackList,
                verbose: int = 1) -> GPModelPopulation:
@@ -49,26 +55,10 @@ class SMBOModelSelector(ModelSelector):
 
             selected_models = [model for model in population.models if model.evaluated]
             fitness_scores = population.fitness_scores()
-            model_selector = SurrogateEvolutionaryModelSelector(selected_models, fitness_scores, self._x_train,
-                                                                self.covariance_to_info_map,
-                                                                gp_fn=self.gp_fn, gp_args=self.gp_args,
-                                                                **self.model_selector_args)
 
-            model_selector.train(self._x_train, self._y_train, eval_budget=self.eval_budget_low_level, verbose=0)
-            new_models = model_selector.selected_models
-
-            all_fitness_scores_zero = all(m.score == 0 for m in new_models)
-            if all_fitness_scores_zero:
-                warnings.warn('All acquisition scores are 0. Be sure this is correct behavior. Reverting to random '
-                              'selection of candidate models.')
-                pool = [m for m in new_models if m not in selected_models]
-                assert len(pool) > 0
-                next_model = np.random.choice(pool)
-            else:
-                next_model_index = int(np.argmax([model.score for model in new_models]))
-                next_model = new_models[next_model_index]
-            # Reinitialize to get rid of acquisition score.
-            next_model = GPModel(next_model.covariance, next_model.likelihood)
+            next_model = self.strategy.query(selected_models, fitness_scores, self._x_train, self._y_train,
+                                             self.eval_budget_low_level, self.gp_fn, self.gp_args,
+                                             **self.model_selector_args)
 
             population.update([next_model])
 
@@ -86,15 +76,29 @@ class SMBOModelSelector(ModelSelector):
                     verbose: int = 0) -> ActiveModelPopulation:
         """Initialize models."""
         population = ActiveModelPopulation()
-        self.active_models = ActiveModels(max_n_models=1000)
+        self.active_models = ActiveModels(self.max_n_models)
 
         initial_candidates = self._get_initial_candidates()
 
         population.update(initial_candidates)
-        initial_candidate_indices = self.active_models.update(initial_candidates)
 
-        no_duplicates = len(initial_candidate_indices) == len(initial_candidates)
+        candidates_copies = [GPModel(candidate.covariance, candidate.likelihood) for candidate in initial_candidates]
+        initial_candidate_indices = self.active_models.update(candidates_copies)
+        no_duplicates = len(initial_candidate_indices) == len(candidates_copies)
         assert no_duplicates
+
+        # init kernel builder
+        self.kernel_builder = CorrelationDistanceBuilder(self.noise_prior,
+                                                         self.num_samples,
+                                                         self.max_n_hyperparameters,
+                                                         self.max_n_models,
+                                                         self.active_models,
+                                                         initial_candidate_indices,
+                                                         self._x_train)
+        # init strategy
+        kernel_kernel_hyperpriors = self.grammar.hyperpriors
+        self.strategy = BayesOptGPStrategy(self.active_models, self.acquisition_fn, self.kernel_builder,
+                                           kernel_kernel_hyperpriors)
 
         if verbose == 3:
             pretty_print_gp_models(population.models, 'Initial candidate')
